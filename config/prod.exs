22a02d69d8e57108f16d3a80e5a74431f6b295c3
Jerry Situ <jerrysitu@gmail.com>
2020-12-17 19:21:05 -0800

    blah

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :audrey, AudreyWeb.Endpoint,
   url: [scheme: "https", host: System.get_env("audrey-staging.gigalixirapp.com", ""), port: 443],
   server: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  check_origin: ["https://audrey-staging.gigalixirapp.com"]
+  check_origin: ["https://audrey-staging.gigalixirapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
